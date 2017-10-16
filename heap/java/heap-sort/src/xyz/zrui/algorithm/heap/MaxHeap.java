284c88c92a0ba4536f24cf94b4a74cbd96a30ddf
zhourui <zhou7rui@gmail.com>
2017-10-16 23:35:47 +0800

    完善最大堆 -》Java版

--- heap/java/heap-sort/src/xyz/zrui/algorithm/heap/MaxHeap.java
+++ heap/java/heap-sort/src/xyz/zrui/algorithm/heap/MaxHeap.java
@@ -2,16 +2,16 @@ package xyz.zrui.algorithm.heap;
 
 public class MaxHeap<T extends Comparable> {
 
-    private T[] data;
+    protected T[] data;
 
-    private int count;
+    protected int count;
 
-    private int capacity;
+    protected int capacity;
 
     //构造函数， 构造一个空的堆，可容纳capacity个元素
     public MaxHeap(int capacity){
 
-        this.data = (T[]) new Object[capacity+1];
+        this.data = (T[]) new Comparable[capacity+1];
 
         this.count = 0;
 
@@ -41,6 +41,34 @@ public class MaxHeap<T extends Comparable> {
 
     }
 
+    private void shiftDown(int k){
+
+        while (2 * k <= count){
+
+            int j = 2 * k;
+
+            if (j + 1 <= count && data[j + 1].compareTo(data[j]) > 0){
+                j += 1;
+            }
+            if (data[k].compareTo(data[j]) > 0){
+                break;
+            }
+            swap(data,j,k);
+            k = j;
+        }
+
+
+
+    }
+
+    public T extractMax(){
+        T ret = data[1];
+        swap(data,count,1);
+        count --;
+        shiftDown(1);
+        return ret;
+    }
+
     /**
      * 交换数组间的位置
      *
@@ -54,6 +82,18 @@ public class MaxHeap<T extends Comparable> {
         arr[j] = t;
     }
 
+
+    // 测试 MaxHeap
+    public static void main(String[] args) {
+
+        MaxHeap<Integer> maxHeap = new MaxHeap<Integer>(100);
+        int N = 50; // 堆中元素个数
+        int M = 100; // 堆中元素取值范围[0, M)
+        for( int i = 0 ; i < N ; i ++ )
+            maxHeap.insert( new Integer((int)(Math.random() * M)) );
+        System.out.println(maxHeap.size());
+
+    }
 
 
 }
