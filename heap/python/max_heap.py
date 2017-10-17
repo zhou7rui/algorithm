111d89404d0b0dc99468cef40bad88cdcad0282d
zhourui <zhou7rui@gmail.com>
2017-10-18 00:56:05 +0800

    最大堆 修改构造方法

--- heap/python/max_heap.py
+++ heap/python/max_heap.py
@@ -16,21 +16,18 @@ import random
 
 class Maxheap(object):
 
-    def __init__(self,cpacity):
+    def __init__(self,cpacity,arr = None):
+
+        self.data = [None] * (cpacity + 1)
         self.cpacity = cpacity
-        self.count = 0
-        self.data = [None] * cpacity
-
-    # def __init__(self,arr,n):
-    #     self.data = [n for n in arr]
-    #     self.count = len(arr)
-    #     self.cpacity = len(arr)
-    #     for i in range(self.count / 2,0, -1):
-    #         self.__shifDown(i)
-
-    def __init__(self):
-        pass
-
+        if arr is None:
+            self.count = 0
+        else:
+            for i in range(0,cpacity):
+                self.data[i + 1]= arr[i]
+            self.count = cpacity;
+            for i in range(self.count / 2, 0, -1):
+                self.__shifDown(i)
 
     def size(self):
         return self.count
@@ -40,7 +37,7 @@ class Maxheap(object):
 
     def __shiftUp(self,k):
 
-        while k > 1 and self.data[k] < self.data[k / 2]:
+        while k > 1 and self.data[k] > self.data[k / 2]:
             self.data[k],self.data[k / 2] = self.data[k / 2], self.data[k]
             k /= 2
 
@@ -51,23 +48,23 @@ class Maxheap(object):
         self.__shiftUp(self.count)
 
     def __shifDown(self,k):
-        while k * 2 <= count:
+        while k * 2 <= self.count:
 
             j = k * 2
 
-            if count >= j + 1 and self.data[j + 1] > self.data[j]:
+            if self.count >= j + 1 and self.data[j + 1] > self.data[j]:
                 j += 1
 
             if self.data[k] > self.data[j]:
                 break
 
-            self.data[k], self[j] = self.data[j],self.data[k]
+            self.data[k], self.data[j] = self.data[j],self.data[k]
 
             k = j
 
     def extractMax(self):
         ret = self.data[1]
-        self.data[1], self.data[count] = self.data[count], self.data[1]
+        self.data[1], self.data[self.count] = self.data[self.count], self.data[1]
         self.count -= 1
         self.__shifDown(1)
         return ret
@@ -82,8 +79,13 @@ if __name__ == '__main__':
     N = 31
     M = 100
     heap = Maxheap(N)
-    for i in range(1,N):
+    for i in range(0,N):
         k = random.randint(1, M)
         heap.insert(k)
+
+    # arr = [random.randint(1,M) for i in range(N)]
+    # heap = Maxheap(len(arr),arr)
+#[None, 95, 84, 85, 78, 80, 50, 74, 62, 70, 59, 51, 38, 47, 57, 71, 9, 16, 19, 53, 18, 25, 44, 7, 28, 2, 24, 16, 18, 13, 33, 70]
     print(heap.size())
+    print(heap.data)
     print(heap.extractMax())
