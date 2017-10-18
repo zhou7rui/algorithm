c817f27a1c15beb3afd85aa2557629d7bd00e134
zhourui <zhou7rui@gmail.com>
2017-10-19 00:15:30 +0800

    使用最大堆进行排序

---
+++ sorting/python/heap_sort.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*
+
+'''
+  堆排序
+'''
+
+import sort_helper
+
+from max_heap import Maxheap
+import quick_3_sort
+
+
+
+@sort_helper.test_sort
+def heap_sort(arr):
+    heap = Maxheap(len(arr),arr)
+    for i in range(len(arr) - 1,-1,-1):
+        arr[i] = heap.extractMax()
+
+def __shifDown(arr,n,k):
+
+    while (2 * k + 1 < n ):
+        j = 2 * k + 1;
+        if j + 1 < n and arr[j + 1] > arr[j] :
+            j += 1
+        if arr[k] >= arr[j]:
+            break
+        arr[j],arr[k] = arr[k],arr[j]
+        k = j
+
+
+def sort(arr):
+
+    n = len(arr)
+    for i in range((n + 1) / 2,-1,-1):
+        __shifDown(arr,n,i)
+
+
+    for i in range(n - 1,0, -1):
+        arr[0], arr[i] = arr[i],arr[0]
+        __shifDown(arr,i,0)
+
+    print(arr)
+
+
+
+
+if __name__ == '__main__':
+    a = sort_helper.generate_randoma_array(100,1,99)
+    print(a)
+    # b = a[:]
+    # heap_sort(a)
+    # quick_3_sort.sort(b)
+    sort(a)
---
+++ sorting/python/max_heap.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*
+
+'''
+                        最大堆实现
+                           98
+                /                      \
+               96                      84
+          /          \            /          \
+         92          82          78          47
+       /    \      /    \      /    \      /    \
+      33    26    51    85    50    15    44    60
+     / \   / \   / \   / \   / \   / \   / \   / \
+    40 51 98 51 7  17 94 82 32 21 64 60 7  44 63 63
+'''
+import random
+
+class Maxheap(object):
+
+    def __init__(self,cpacity,arr = None):
+
+        self.data = [None] * (cpacity + 1)
+        self.cpacity = cpacity
+        if arr is None:
+            self.count = 0
+        else:
+            for i in range(0,cpacity):
+                self.data[i + 1]= arr[i]
+            self.count = cpacity;
+            for i in range(self.count / 2, 0, -1):
+                self.__shifDown(i)
+
+    def size(self):
+        return self.count
+
+    def isEmpty(self):
+        return self.count == 0
+
+    def __shiftUp(self,k):
+
+        while k > 1 and self.data[k] > self.data[k / 2]:
+            self.data[k],self.data[k / 2] = self.data[k / 2], self.data[k]
+            k /= 2
+
+
+    def insert(self,data):
+        self.data[self.count + 1] = data
+        self.count += 1
+        self.__shiftUp(self.count)
+
+    def __shifDown(self,k):
+        while k * 2 <= self.count:
+
+            j = k * 2
+
+            if self.count >= j + 1 and self.data[j + 1] > self.data[j]:
+                j += 1
+
+            if self.data[k] > self.data[j]:
+                break
+
+            self.data[k], self.data[j] = self.data[j],self.data[k]
+
+            k = j
+
+    def extractMax(self):
+        ret = self.data[1]
+        self.data[1], self.data[self.count] = self.data[self.count], self.data[1]
+        self.count -= 1
+        self.__shifDown(1)
+        return ret
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    N = 31
+    M = 100
+    heap = Maxheap(N)
+    for i in range(0,N):
+        k = random.randint(1, M)
+        heap.insert(k)
+
+    # arr = [random.randint(1,M) for i in range(N)]
+    # heap = Maxheap(len(arr),arr)
+    print(heap.size())
+    print(heap.data)
+    print(heap.extractMax())
