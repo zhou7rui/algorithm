c2910ffe7e3558901cc632d4dcec8e2e1fac15f3
zhourui <zhou7rui@gmail.com>
2018-05-22 00:55:23 +0800

    冒泡排序优化 go实现

--- sorting/golang/bubble_sort.go
+++ sorting/golang/bubble_sort.go
@@ -14,6 +14,8 @@ func bubbleSort(arr []int, n int) []int {
 }
 
 
+// 冒泡排序优化思路1
+// 设置开关，当第二层循环不在进行交换时说明排序完成 提前结束外层循环
 func bubbleSort1(arr []int, n int) []int {
 	for i := 0; i < n-1; i++ {
 		flag := true
@@ -30,9 +32,30 @@ func bubbleSort1(arr []int, n int) []int {
 	return arr
 }
 
+// 冒泡排序优化思路2
+// 记录最后一次交换位置， 上层循环从最后一次位置开始
+func bubbleSort2(arr []int, n int) []int {
+
+	flag := n
+	for flag > 0 {
+		n = flag
+		flag = 0
+		for i := 1; i < n; i ++ {
+			if arr[i] < arr[i - 1] {
+				arr[i], arr[i - 1 ] = arr[i - 1], arr[i]
+				flag = i
+			}
+		}
+
+	}
+
+
+	return nil
+
+}
 
 func main() {
 	arr := help.GenRanArray(10000, 1, 9999)
 	// help.ArrayPrint(arr)
-	help.TestSort(bubbleSort1)(arr, len(arr))
+	help.TestSort(bubbleSort2)(arr, len(arr))
 }
